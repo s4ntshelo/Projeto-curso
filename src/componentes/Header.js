7bea05fa08d297c2f458b2686da940262e59e80d
s4ntshelo <heloa.vitoria.santos@escola.pr.gov.br>
2026-06-09 12:32:29 -0300

    Add files via upload

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -4,21 +4,21 @@ import { Link } from "react-router-dom";
 function Header() {
 
     return (
-        <>
-           <div className="topo">
+        <header>
+           <div className="headerTopo">
             <h1>Curso Técnico em Desenvolvimento De Sistemas</h1>
             <div/>
-                <nav className="menu">
+                <nav className="headerMenu">
                     <ul>
                         <li><Link to = "/" >Home</Link></li>
                         <li><Link to = "/sobreCurso" >Curso</Link></li>
                         <li><Link to = "/disciplinasCurriculares" >Disciplinas Básicas</Link></li>
                         <li><Link to = "/disciplinasTecnicas" >Disciplinas Técnicas</Link></li>
-                        <li><Link to = "/equipe" >Eqipe</Link></li>
+                        <li><Link to = "/equipe" >Equipe</Link></li>
                     </ul>
                 </nav>
            </div>
-       </>
+        </header>
     )
 }
 
